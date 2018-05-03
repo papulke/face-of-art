dc6837c6832cd087288bf19b6b424ac7669bb0ee
papulke <papulke@gmail.com>
2018-05-03 12:18:08 +0300

    added texture augmentation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from deep_heatmaps_model_primary_valid import DeepHeatmapsModel
+from deep_heatmaps_model_primary_fusion import DeepHeatmapsModel
 
 # data_dir ='/mnt/External1/Yarden/deep_face_heatmaps/data/conventional_landmark_detection_dataset/'
 data_dir = '/Users/arik/Dropbox/a_mac_thesis/face_heatmap_networks/conventional_landmark_detection_dataset/'
@@ -25,10 +25,10 @@ def main(_):
     if not tf.gfile.Exists(FLAGS.save_log_path):
         tf.gfile.MakeDirs(FLAGS.save_log_path)
 
-    model = DeepHeatmapsModel(mode=FLAGS.mode, train_iter=80000, learning_rate=1e-4, image_size=256, c_dim=3,batch_size=4,
-                              num_landmarks=68, augment=True, img_path=FLAGS.img_path,
+    model = DeepHeatmapsModel(mode=FLAGS.mode, train_iter=80000, learning_rate=1e-6, image_size=256, c_dim=3,batch_size=4,
+                              num_landmarks=68, augment_basic=True, augment_texture=True, img_path=FLAGS.img_path,
                               save_log_path=FLAGS.save_log_path, save_sample_path=FLAGS.save_sample_path,
-                              save_model_path=FLAGS.save_model_path, test_model_path = 'model/deep_heatmaps-56000')
+                              save_model_path=FLAGS.save_model_path, test_model_path = 'model/deep_heatmaps-5')
 
     if FLAGS.mode == 'TRAIN':
         model.train()
