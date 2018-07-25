fix NME calculation + minor stuff

data_loading_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from skimage.color import gray2rgb
 
 
 def train_val_shuffle_inds_per_epoch(valid_inds, train_inds, train_iter, batch_size, log_path, save_log=True):
@@ -62,7 +63,10 @@ def load_images_landmarks_maps(img_list, batch_inds, primary=False, image_size=2
 
     for ind, img in enumerate(batch_menpo_images):
 
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
 
         if primary:
             lms = img.resize([image_size / 4, image_size / 4]).landmarks['PTS'].points
@@ -104,7 +108,10 @@ def load_images(img_list, batch_inds, image_size=256, c_dim=3, scale='255'):
         images = np.zeros([num_inputs, image_size, image_size, c_dim]).astype('float32')
 
         for ind, img in enumerate(batch_menpo_images):
-            images[ind, :, :, :] = img.pixels_with_channels_at_back()
+            if img.n_channels < 3 and c_dim == 3:
+                images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+            else:
+                images[ind, :, :, :] = img.pixels_with_channels_at_back()
 
         if scale is '255':
             images *= 255  # SAME AS ECT?
@@ -127,7 +134,10 @@ def load_images_landmarks(img_list, batch_inds, primary=False, image_size=256, c
 
     for ind, img in enumerate(batch_menpo_images):
 
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
         lms_small = img.resize([image_size/4, image_size / 4]).landmarks['PTS'].points
         landmarks_small[ind, :, :] = np.minimum(lms_small, image_size/4 - 1)
 
@@ -184,10 +194,14 @@ def load_images_landmarks_maps_alloc_once(img_list, batch_inds, images, maps_sma
                                           save_landmarks=False):
 
     batch_menpo_images = img_list[batch_inds]
+    c_dim = images.shape[-1]
 
     for ind, img in enumerate(batch_menpo_images):
 
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
 
         if primary:
             lms = img.resize([image_size / 4, image_size / 4]).landmarks['PTS'].points
@@ -241,10 +255,13 @@ def create_heat_maps_base_alloc_once(
 def load_images_landmarks_alloc_once(
         img_list, batch_inds, images, landmarks_small, landmarks, primary=False, image_size=256, scale='255'):
     batch_menpo_images = img_list[batch_inds]
+    c_dim = images.shape[-1]
 
     for ind, img in enumerate(batch_menpo_images):
-
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
         lms_small = img.resize([image_size / 4, image_size / 4]).landmarks['PTS'].points
         landmarks_small[ind, :, :] = np.minimum(lms_small, image_size / 4 - 1)
 
@@ -319,6 +336,7 @@ def load_images_landmarks_approx_maps_alloc_once(
         scale='255', gauss_filt_large=None, gauss_filt_small=None, win_mult=3.5, sigma=6, save_landmarks=False):
 
     batch_menpo_images = img_list[batch_inds]
+    c_dim = images.shape[-1]
 
     if primary:
         win_size_small = int(win_mult * sigma)
@@ -338,8 +356,10 @@ def load_images_landmarks_approx_maps_alloc_once(
             gauss_filt_large = (8. / 3) * sigma * gaussian(x_large, y_large, win_size_large, win_size_large, sigma=sigma)  # copied from ECT
 
     for ind, img in enumerate(batch_menpo_images):
-
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
 
         if primary:
             lms = img.resize([image_size / 4, image_size / 4]).landmarks['PTS'].points
@@ -409,8 +429,10 @@ def load_images_landmarks_approx_maps(
                                                            sigma=sigma)  # copied from ECT
 
     for ind, img in enumerate(batch_menpo_images):
-
-        images[ind, :, :, :] = img.pixels_with_channels_at_back()
+        if img.n_channels < 3 and c_dim == 3:
+            images[ind, :, :, :] = gray2rgb(img.pixels_with_channels_at_back())
+        else:
+            images[ind, :, :, :] = img.pixels_with_channels_at_back()
 
         if primary:
             lms = img.resize([image_size / 4, image_size / 4]).landmarks['PTS'].points

deep_heatmaps_model_fusion_net.py
@@ -20,8 +20,8 @@ class DeepHeatmapsModel(object):
                  augment_basic=True, basic_start=0, augment_texture=False, p_texture=0., augment_geom=False,
                  p_geom=0., artistic_start=0, artistic_step=2, img_path='data',
                  save_log_path='logs', save_sample_path='sample', save_model_path='model', test_data='full',
-                 test_model_path='model/deep_heatmaps-1000', load_pretrain=False, load_primary_only=True,
-                 pre_train_path='saved_models/model/deep_heatmaps-50000'):
+                 test_model_path='model/deep_heatmaps-1000', load_pretrain=False, load_primary_only=False,
+                 pre_train_path='saved_models/model/deep_heatmaps-50000', menpo_verbose=True):
 
         # values to print to save parameter:
 
@@ -138,7 +138,7 @@ class DeepHeatmapsModel(object):
             margin=margin, bb_type=bb_type, test_data=self.test_data,
             augment_basic=(augment_basic and basic_start == 0),
             augment_texture=(augment_texture and artistic_start == 0 and p_texture > 0.), p_texture=p_texture,
-            augment_geom=(augment_geom and artistic_start == 0 and p_geom > 0.), p_geom=p_geom)
+            augment_geom=(augment_geom and artistic_start == 0 and p_geom > 0.), p_geom=p_geom, verbose=menpo_verbose)
 
         if mode == 'TRAIN':
 
@@ -156,7 +156,8 @@ class DeepHeatmapsModel(object):
                     self.valid_bb_dictionary = load_bb_dictionary(self.bb_dir, 'TEST', test_data=self.valid_data)
                     self.valid_img_menpo_list = load_menpo_image_list(
                         img_path, train_crop_dir, img_dir_ns, 'TEST', bb_dictionary=self.valid_bb_dictionary,
-                        image_size=self.image_size, margin=margin, bb_type=bb_type, test_data=self.valid_data)
+                        image_size=self.image_size, margin=margin, bb_type=bb_type, test_data=self.valid_data,
+                        verbose=menpo_verbose)
 
                     np.random.seed(0)
                     self.val_inds = np.arange(len(self.valid_img_menpo_list))
@@ -383,31 +384,24 @@ class DeepHeatmapsModel(object):
 
     def create_loss_ops(self):
 
-        def l2_loss_norm_eyes(pred_landmarks, real_landmarks, normalize=True, name='NME_loss'):
+        def l2_loss_norm_eyes(pred_landmarks, real_landmarks, normalize=True, name='NME'):
 
             with tf.name_scope(name):
-                with tf.name_scope('real_pred_landmarks_diff'):
-                    landmarks_diff = pred_landmarks - real_landmarks
-
+                with tf.name_scope('real_pred_landmarks_rmse'):
+                    landmarks_rms_err = tf.reduce_mean(
+                        tf.sqrt(tf.reduce_sum(tf.square(pred_landmarks - real_landmarks), axis=2)), axis=1)
                 if normalize:
                     with tf.name_scope('inter_pupil_dist'):
-                        with tf.name_scope('left_eye'):
+                        with tf.name_scope('left_eye_center'):
                             p1 = tf.reduce_mean(tf.slice(real_landmarks, [0, 42, 0], [-1, 6, 2]), axis=1)
-                        with tf.name_scope('right_eye'):
+                        with tf.name_scope('right_eye_center'):
                             p2 = tf.reduce_mean(tf.slice(real_landmarks, [0, 36, 0], [-1, 6, 2]), axis=1)
-                        eps = 1e-6
-                        eye_dist = tf.expand_dims(tf.expand_dims(
-                            tf.sqrt(tf.reduce_sum(tf.square(p1 - p2), axis=1)) + eps, axis=1), axis=1)
 
-                    norm_landmarks_diff = landmarks_diff / eye_dist
-                    l2_landmarks_norm = tf.reduce_mean(tf.square(norm_landmarks_diff))
+                        eye_dist = tf.sqrt(tf.reduce_sum(tf.square(p1 - p2), axis=1))
 
-                    out = l2_landmarks_norm
+                    return landmarks_rms_err / eye_dist
                 else:
-                    l2_landmarks = tf.reduce_mean(tf.square(landmarks_diff))
-                    out = l2_landmarks
-
-                return out
+                    return landmarks_rms_err
 
         if self.mode is 'TRAIN':
             primary_maps_diff = self.pred_hm_p - self.heatmaps_small
@@ -418,13 +412,14 @@ class DeepHeatmapsModel(object):
             self.total_loss = self.l_weight_primary * self.l2_primary + self.l_weight_fusion * self.l2_fusion
 
             if self.compute_nme:
-                self.nme_loss = l2_loss_norm_eyes(self.train_pred_lms, self.train_lms)
+                self.nme_loss = tf.reduce_mean(l2_loss_norm_eyes(self.train_pred_lms, self.train_lms))
 
             if self.valid_size > 0 and self.compute_nme:
-                self.valid_nme_loss = l2_loss_norm_eyes(self.valid_pred_lms,self.valid_lms)
+                self.valid_nme_loss = tf.reduce_mean(l2_loss_norm_eyes(self.valid_pred_lms,self.valid_lms))
 
         elif self.mode == 'TEST' and self.compute_nme:
-            self.nme_loss = l2_loss_norm_eyes(self.pred_lms, self.lms)
+            self.nme_per_image = l2_loss_norm_eyes(self.pred_lms, self.lms)
+            self.nme_loss = tf.reduce_mean(self.nme_per_image)
 
     def predict_landmarks_in_batches(self, image_paths, session):
 