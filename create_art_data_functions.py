3781d9793d70633e9bb6d391014dd5001565ab00
papulke <papulke@gmail.com>
2019-01-31 11:30:20 +0200

    add upsample loss, add option for image warping wo black borders

--- create_art_data_functions.py
+++ create_art_data_functions.py
@@ -1,5 +1,77 @@
 from menpo_functions import *
 from data_loading_functions import *
+from menpo.shape import bounding_box
+from menpo.transform import Translation, Rotation
+
+
+
+def augment_face_image(img, image_size=256, crop_size=248, angle_range=30, flip=True):
+    """basic image augmentation: random crop, rotation and horizontal flip"""
+
+    #from menpo
+    def round_image_shape(shape, round):
+        if round not in ['ceil', 'round', 'floor']:
+            raise ValueError('round must be either ceil, round or floor')
+        # Ensure that the '+' operator means concatenate tuples
+        return tuple(getattr(np, round)(shape).astype(np.int))
+
+    # taken from MDM
+    def mirror_landmarks_68(lms, im_size):
+        return PointCloud(abs(np.array([0, im_size[1]]) - lms.as_vector(
+        ).reshape(-1, 2))[mirrored_parts_68])
+
+    # taken from MDM
+    def mirror_image(im):
+        im = im.copy()
+        im.pixels = im.pixels[..., ::-1].copy()
+
+        for group in im.landmarks:
+            lms = im.landmarks[group]
+            if lms.points.shape[0] == 68:
+                im.landmarks[group] = mirror_landmarks_68(lms, im.shape)
+
+        return im
+
+    flip_rand = np.random.random() > 0.5
+    #     rot_rand = np.random.random() > 0.5
+    #     crop_rand = np.random.random() > 0.5
+    rot_rand = True  # like ECT
+    crop_rand = True  # like ECT
+
+    if crop_rand:
+        lim = image_size - crop_size
+        min_crop_inds = np.random.randint(0, lim, 2)
+        max_crop_inds = min_crop_inds + crop_size
+        img = img.crop(min_crop_inds, max_crop_inds)
+
+    if flip and flip_rand:
+        img = mirror_image(img)
+
+    if rot_rand:
+        rot_angle = 2 * angle_range * np.random.random_sample() - angle_range
+        # img = img.rotate_ccw_about_centre(rot_angle)
+
+        # Get image's bounding box coordinates
+        bbox = bounding_box((0, 0), [img.shape[0] - 1, img.shape[1] - 1])
+        # Translate to origin and rotate counter-clockwise
+        trans = Translation(-img.centre(),
+                            skip_checks=True).compose_before(
+            Rotation.init_from_2d_ccw_angle(rot_angle, degrees=True))
+        rotated_bbox = trans.apply(bbox)
+        # Create new translation so that min bbox values go to 0
+        t = Translation(-rotated_bbox.bounds()[0])
+        trans.compose_before_inplace(t)
+        rotated_bbox = trans.apply(bbox)
+        # Output image's shape is the range of the rotated bounding box
+        # while respecting the users rounding preference.
+        shape = round_image_shape(rotated_bbox.range() + 1, 'round')
+
+        img = img.warp_to_shape(
+            shape, trans.pseudoinverse(), warp_landmarks=True, mode='nearest')
+
+    img = img.resize([image_size, image_size])
+
+    return img
 
 
 def augment_menpo_img_ns(img, img_dir_ns, p_ns=0, ns_ind=None):
